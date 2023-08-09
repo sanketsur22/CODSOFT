262de8b0ea0dbfadcb0b5570ffc2a2e5eb523476
sanketsur22 <sanketsur22@gmail.com>
2023-08-09 21:25:15 +0530

    Internship

--- AtmInterface.java
+++ AtmInterface.java
@@ -1,73 +1,58 @@
 import java.util.*;
 
-class ATM{
-    float Balance;
+/* Withdraw , Deposit , Checkbalance */
 
-    public void menu(float x){
-        System.out.println();
-        System.out.println("1. Check Account Balance");
-        System.out.println("2. Withdraw Money");
-        System.out.println("3.Deposit Money");
-        System.out.println("4. Exit");
-        System.out.println("Enter your choice");
 
-        Scanner sc = new Scanner(System.in);
-        int option = sc.nextInt(); 
-
-        switch(option){
-
-            case 1:
-                checkBalance();
-                break;
-
-            case 2:
-                withdrawMoney(x);
-                break;
-            
-            case 3:
-                depositMoney(x);
-                break;
-
-            case 4:
-                break;
-        }
-    
-
-    }
-
-    public void checkBalance(){
-        System.out.println("Balance : " + Balance);
-        menu();
-    }
-
-    public void withdrawMoney(float amt){
-        System.out.println("Enter amount to Withdraw");
-        
-        
-        if(amt>Balance){
-            System.out.println("Insufficient Balane");
-        }
-        else{
-            Balance -= amt;
-            System.out.println("Withdrawl successful");
-        }
-        menu();
-    }
-
-    public void depositMoney(float amt){
-        System.out.println("Enter the amount");
-        Balance += amt;
-        return amt;
-        System.out.println("Money Deposited Successfully");
-        menu();
-    }
+public class AtmInterface
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    Bank bankDetails = new Bank();
+	    while(true) {
+	        System.out.println();
+	        System.out.println("Enter 1 for deposit ");
+	        System.out.println("Enter 2 for withdraw ");
+	        System.out.println("Enter 3 to check balance ");
+	        System.out.println("Enter 4 to exit ");
+	        System.out.println();
+	        int n = sc.nextInt();
+	        System.out.println();
+	        switch(n) {
+	            case 1:
+	                System.out.println("Enter the amount to be deposited: ");
+	                double amount = sc.nextDouble();
+	                bankDetails.deposit(amount);
+	                break;
+	            case 2:
+	                System.out.println("Enter the amount to be withdrawn: ");
+	                double amount2 = sc.nextDouble();
+	                bankDetails.withdraw(amount2);
+	                break;
+	            case 3:
+	                System.out.println("The current amount in your bank account is : " +bankDetails.checkBalance());
+	                break;
+	            case 4:
+	                System.out.println("Thank you for visiting our bank !!");
+	                System.exit(1);
+	        }
+	    }
+	}
 }
-
-public class AtmInterface {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        float amount = sc.nextFloat();
-        ATM obj = new ATM();
-        obj.menu(amount);
+class Bank {
+    double accBalance;
+    public void withdraw(double amount) {
+        if(amount > accBalance) {
+            System.out.println("Insufficient Balance !!");
+            return;
+        }
+        accBalance -= amount;
+        System.out.println("Amount was withdrawn successfully !!");
+    }
+    public void deposit(double amount) {
+        accBalance += amount;
+        System.out.println("Amount was deposited successfully !!");
+    }
+    public double checkBalance() {
+        return accBalance;
     }
 }
